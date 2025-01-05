8215b4e049102b0b4e3628b0988d09c39dcb53af
Stacy Chen <chens18@bxscience.edu>
2025-01-05 13:54:49 -0500

    trying to test branch protection

--- src/main/java/frc/robot/commands/Autoas.java
+++ src/main/java/frc/robot/commands/Autoas.java
@@ -7,7 +7,7 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 
-public final class Autos {
+public final class Autoas {
   /** Example static factory for an autonomous command. */
   // public static Command exampleAuto(ExampleSubsystem subsystem) {
     
