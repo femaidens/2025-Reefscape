62767bbd830e6377f2285d43d8c58b6516774400
Stacy Chen <chens18@bxscience.edu>
2025-01-08 17:51:25 -0500

    changed configs for 2025

---
+++ src/main/java/frc/robot/Ports.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+public class Ports {
+    public class DrivetrainPorts {
+        public static final int FRONT_LEFT_DRIVE = 0;
+        public static final int FRONT_RIGHT_DRIVE = 0;
+        public static final int REAR_LEFT_DRIVE = 0;
+        public static final int REAR_RIGHT_DRIVE = 0;
+
+        public static final int FRONT_LEFT_TURN = 0;
+        public static final int FRONT_RIGHT_TURN = 0;
+        public static final int REAR_LEFT_TURN = 0;
+        public static final int REAR_RIGHT_TURN = 0; 
+    }
+}
