d3f5b82390a4aec32f3147c1f41d74b2100e5fac
Emily Chen <emilyc101708@gmail.com>
2025-01-14 17:44:13 -0500

    reformatted a little and got my encoder!!! just figuring out how to config it....

--- src/main/java/frc/robot/subsystems/ModuleKraken.java
+++ src/main/java/frc/robot/subsystems/ModuleKraken.java
@@ -3,10 +3,14 @@ package frc.robot.subsystems;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.Slot1Configs;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.CANcoder;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.swerve.utility.PhoenixPIDController;
 
 import frc.robot.subsystems.DriveConstants.Translation;
+import frc.robot.subsystems.DriveConstants.Translation.PID;
 import frc.robot.subsystems.DriveConstants.Turn;
 
 
@@ -14,38 +18,42 @@ public class ModuleKraken {
     private final TalonFX driveMotor; 
     private final TalonFX turnMotor; 
     
-    // I give up on the encoders for now :/ 
-    //private final ParentDevice driveEncoder;
-    //private final ParentDevice turnEncoder; 
+    // This is both an absolute and relative encoder 
+    private final CANcoder encoder;
     
     //PID? 
-    private final Slot0Configs slot0Turn; 
-    private final Slot1Configs slot1Drive; 
-       
+    private PhoenixPIDController drivePidController; 
+    private PhoenixPIDController turnPidController; 
 
+    
     // IDK what canbus we use so i just set it to the robot rio for now. 
     //THIS SHOULD BE SUBJECT TO CHANGE?! 
     public ModuleKraken(int driveID, int turnID){
         driveMotor = new TalonFX(driveID, "rio"); 
-        driveMotor.setNeutralMode(NeutralModeValue.Brake); 
-        driveMotor.getConfigurator().apply(new CurrentLimitsConfigs().withSupplyCurrentLimit(Translation.CURRENT_LIMIT)); 
-
+        configureTalon(driveMotor); 
 
         turnMotor = new TalonFX(turnID, "rio"); 
-        turnMotor.setNeutralMode(NeutralModeValue.Brake); 
-        turnMotor.getConfigurator().apply(new CurrentLimitsConfigs().withSupplyCurrentLimit(Turn.CURRENT_LIMIT)); 
+        configureTalon(turnMotor); 
 
-        // Highkey don't know how to work the encoders broooooooooooooooooo...
-        ///driveEncoder = new ParentDevice(driveID, "encoder", "rio"); 
-        //turnEncoder.create(driveID, "rio");
+        drivePidController = new PhoenixPIDController(Translation.PID.P, Translation.PID.I, Translation.PID.D); 
+        turnPidController = new PhoenixPIDController(Turn.PID.P,Turn.PID.I, Turn.PID.D);
 
-        //This has all the PID values, but IDK how to calculate with them... erm 
-        slot0Turn = new Slot0Configs().withKP(Translation.PID.P).withKI(Translation.PID.I).withKD(Translation.PID.D);
-        slot1Drive = new Slot1Configs().withKP(Translation.PID.P).withKI(Translation.PID.I).withKD(Translation.PID.D); 
-
+        encoder = new CANcoder(0, "rio"); 
+        
          
     }
 
+    /**
+     * This configures a TalonFX motor!!! 
+     * @param 
+     */
+
+     public static void configureTalon(TalonFX motor){
+        motor.setNeutralMode(NeutralModeValue.Brake); 
+        motor.getConfigurator().apply(new CurrentLimitsConfigs().withSupplyCurrentLimit(Translation.CURRENT_LIMIT)); 
+     }
+
+     
 
 
 
