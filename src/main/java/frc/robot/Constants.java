bd8212cd03d9318f0c19368edc934334fb5f5b05
Olivia Wang <ow98914@gmail.com>
2025-01-17 15:59:47 -0500

    added constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -16,4 +16,8 @@ public final class Constants {
   public static class OperatorConstants {
     public static final int kDriverControllerPort = 0;
   }
+
+  public static class OutakeConstants {
+    public static final double motorSpeed = 0;
+  }
 }
