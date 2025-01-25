fb6ce61aa8fbab4b5c9ef572008acba5f407de14
Tsering Lama <phinjohyolmo@phinjos-MacBook-Pro.local>
2025-01-25 13:48:24 -0500

    retry

--- src/main/java/frc/robot/subsystems/climb.java
+++ src/main/java/frc/robot/subsystems/climb.java
@@ -2,13 +2,15 @@ package frc.robot.subsystems;
 import frc.robot.Ports;
 import frc.robot.Constants.ClimbConstants;
 
-
-import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.RelativeEncoder;
 import frc.robot.Constants;
 
 import com.ctre.phoenix6.hardware.core.CoreTalonFX;
+import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.hardware.TalonFX;
 
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -16,15 +18,11 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class climb extends SubsystemBase {
     private final TalonFX LEADER = new TalonFX(Ports.LEADER_PORT);
     private final TalonFX FOLLOWER = new TalonFX (Ports.FOLLOWER_PORT);
-
-    AbsoluteEncoder Encoder;
-
-    public climb(){
-        Encoder = LEADER.getAbsoluteEncoder();
-    }
+    Object leaderMotor;
 
     double MAXPosition = 360.0;
     double MINPosition = 0; //subject to change, need to change to rotation??
+
 
 
 
@@ -37,8 +35,9 @@ public class climb extends SubsystemBase {
   }
 
   public void climbUP(){
-    {
-        if (Encoder.getPosition () < MAXPosition) {
+    double currentPosition = leaderMotor.getSelectedEncoderPosition();
+
+        if (currentPosition < MAXPosition) {
             LEADER.set (ClimbConstants.ClimbSpeed);
             FOLLOWER.set (ClimbConstants.ClimbSpeed);
         } else {
@@ -47,11 +46,10 @@ public class climb extends SubsystemBase {
         }
 
     }
-  }
 
   public void climbDOWN (){
-    {
-        if (Encoder.getPosition () > MINPosition){
+        double currentPosition = leaderMotor.getSelectedEncoderPosition();
+        if (currentPosition > MINPosition){
             LEADER.set(-ClimbConstants.ClimbSpeed);
             FOLLOWER.set(-ClimbConstants.ClimbSpeed);
         } else {
@@ -59,17 +57,10 @@ public class climb extends SubsystemBase {
             FOLLOWER.set(0);
         }
     }
-  }
+  
 
   public void stopMotors(){
     LEADER.set(0);
     FOLLOWER.set(0);
   }
-
-
-  
-   
-
-
-
 }
