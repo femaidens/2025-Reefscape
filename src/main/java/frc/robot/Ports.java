efde97a962778bc0c8990498fc266ec9c88f59c3
yujaime <yuj@bxscience.edu>
2025-02-05 16:40:47 -0500

    fixed port/constant errors in intake and climb

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -1,6 +1,13 @@
 package frc.robot;
 
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+
+
 public class Ports {
+
     public class DrivetrainPorts {
         public static final int FRONT_LEFT_DRIVE = 3;
         public static final int REAR_LEFT_DRIVE = 5;
@@ -17,13 +24,7 @@ public class Ports {
         public static final int REAR_LEFT_CANCODER = 2;
         public static final int REAR_RIGHT_CANCODER = 1;
     }
-}// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
-package frc.robot;
-
-public class Ports {
     public class ElevatorPorts{
         public static int MOTOR_PORT = 3;
         public static int BOT_SWITCH = 2;
@@ -45,7 +46,7 @@ public class Ports {
     }
 
     public class ClimbPorts {
-      public static final int LEADER_MOTOR = 0; 
-      public static final int FOLLOWER_MOTOR = 0;
+      public static int LEADER_PORT = 0; 
+      public static int FOLLOWER_PORT = 0;
     }
   }
