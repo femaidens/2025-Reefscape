5eada6b4452a4f5ceb5a04ee661076a081171c54
yujaime <yuj@bxscience.edu>
2025-02-10 16:56:44 -0500

    committing before pulling

--- src/main/java/frc/robot/subsystems/DriveConstants.java
+++ src/main/java/frc/robot/subsystems/DriveConstants.java
@@ -40,7 +40,7 @@ public class DriveConstants {
         public class PID {
             public static final double P = 1;
             public static final double I = 0;
-            public static final double D = 0;
+            public static final double D = 0.1;
         }
         public class FF {
             public static final double S = .18484;
@@ -55,9 +55,9 @@ public class DriveConstants {
         public static final int AVERAGE_DEPTH = 0;
 
         public class PID {
-            public static final double P = .08;
+            public static final double P = .1;
             public static final double I = 0;
-            public static final double D = 0.0;   
+            public static final double D = 0.1;   
         }
         
         public class FF {
