258f07f36d3d9e41c02a27a960036e2442ac44cc
yujaime <yuj@bxscience.edu>
2025-02-12 22:51:43 -0500

    added elevator commands except algaethirdlevel

--- src/main/java/frc/robot/commands/Elevating.java
+++ src/main/java/frc/robot/commands/Elevating.java
@@ -7,40 +7,68 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants;
 import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Outtake;
 
 /** Add your docs here. */
 public class Elevating {
 
     private Elevator elevator;
+    private Outtake outtake;
 
     public Elevating(){
         this.elevator = new Elevator();
+        this.outtake = new Outtake();
     }
     /**
      * @return goes to first level of reef
      */
     public Command firstLevelCmd(){
-        return elevator.setLevel(Constants.ElevatorConstants.SetpointConstants.FIRST_LVL);
+        return 
+            elevator.setLevel(Constants.ElevatorConstants.SetpointConstants.FIRST_LVL)
+            .andThen(outtake.setOuttakeCoralSpeedCmd());
     }
     /**
      * 
      * @return goes to second level of reef
      */
     public Command secondLevelCmd(){
-        return elevator.setLevel(Constants.ElevatorConstants.SetpointConstants.SECOND_LVL);
+        return 
+            elevator.setLevel(Constants.ElevatorConstants.SetpointConstants.SECOND_LVL)
+            .andThen(outtake.setOuttakeCoralSpeedCmd());
     }
     /**
      * 
      * @return goes to third level of reef
      */
     public Command thirdLevelCmd(){
-        return elevator.setLevel(Constants.ElevatorConstants.SetpointConstants.THIRD_LVL);
+        return 
+            elevator.setLevel(Constants.ElevatorConstants.SetpointConstants.THIRD_LVL)
+            .andThen(outtake.setOuttakeCoralSpeedCmd());
     }
     /**
      * 
      * @return goes to the fourth level of reef
      */
     public Command fourthLevelCmd(){
-        return elevator.setLevel(Constants.ElevatorConstants.SetpointConstants.FOURTH_LVL);
+        return 
+            elevator.setLevel(Constants.ElevatorConstants.SetpointConstants.FOURTH_LVL)
+            .andThen(outtake.setOuttakeCoralSpeedCmd());
+    }
+
+    /**
+     * @return goes to algae removal level 2
+     */
+    public Command algaeSecondLevelCmd(){
+        return 
+            elevator.setLevel(Constants.ElevatorConstants.SetpointConstants.ALGAE_SECOND_LVL)
+            .andThen(outtake.setOuttakeCoralSpeedCmd());
+    }
+
+    /**
+     * @return goes to algae removal level 3
+     * someone else should do this one!!!
+     */
+    public Command algaeThirdLevelCmd(){
+        
     }
 }
