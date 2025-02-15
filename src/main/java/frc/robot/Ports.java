8f7814522dac4c2f28318452d5990446f72dfcef
Stacy Chen <chens18@bxscience.edu>
2025-02-15 09:47:25 -0500

    changed ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,12 +3,12 @@ package frc.robot;
 public class Ports {
     public class DrivetrainPorts {
         public static final int FRONT_LEFT_DRIVE = 13;
-        public static final int REAR_LEFT_DRIVE = 12;
+        public static final int REAR_LEFT_DRIVE = 4;
         public static final int FRONT_RIGHT_DRIVE = 11;
         public static final int REAR_RIGHT_DRIVE = 10;
     
         public static final int FRONT_LEFT_TURN = 9;
-        public static final int REAR_LEFT_TURN = 4;
+        public static final int REAR_LEFT_TURN = 12;
         public static final int FRONT_RIGHT_TURN = 15;
         public static final int REAR_RIGHT_TURN = 20;
 
