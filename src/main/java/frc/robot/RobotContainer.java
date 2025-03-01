fb603128b36aa7df2a5203865a4a6cb421ac7fe7
Ngozi Ihesiaba <ihesiaban@bxscience.edu>
2025-03-01 15:27:45 -0500

    actually finished button binding

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,162 +5,211 @@
 package frc.robot;
 
 import frc.robot.Constants.*;
-import frc.robot.commands.Autos;
-// import frc.robot.subsystems.Drive;
-import frc.robot.subsystems.AlgaeIntake;
-import frc.robot.subsystems.AlgaePivot;
-
-import java.util.function.DoubleSupplier;
-
+import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Outtake;
 import edu.wpi.first.math.MathUtil;
-import frc.robot.Ports.JoyPort;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.subsystems.AlgaeIntake;
-import frc.robot.subsystems.Climb;
-import frc.robot.subsystems.Drive;
-import frc.robot.subsystems.Intake;
-import frc.robot.subsystems.Outtake;
-// import frc.robot.commands.Elevating;
-// import frc.robot.commands.AlgaeCmds; 
-import frc.robot.commands.CoralTransition;
-import frc.robot.commands.Elevating;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+        // The robot's subsystems and commands are defined here...
+        // Drive drive = new Drive();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController driveJoy = new CommandXboxController(JoyPort.DRIVER_PORT);
-  private final CommandXboxController operJoy = new CommandXboxController(JoyPort.OPERATOR_PORT);
-  // private final Climb climb;
-  // private final AlgaeCmds algaeCmds = new AlgaeCmds();
-  private final AlgaeIntake algaeIntake;
-  private final AlgaePivot algaePivot;
-  private final Drive drivetrain;
-  private final Elevating elevating;
-  private final CoralTransition coralTransition;
-  private final Intake intake; 
-  private final Outtake outtake;
+        // Replace with CommandPS4Controller or CommandJoystick if needed
+        private final CommandXboxController driveJoy = new CommandXboxController(OperatorConstants.DRIVER_PORT);
+        private final CommandXboxController operJoy = new CommandXboxController(OperatorConstants.OPERATOR_PORT);
+        // private final AlgaeIntake algaeIntake = new AlgaeIntake();
+        // private final AlgaePivot algaePivot = new AlgaePivot();
+        private final Drive drivetrain;
+        private final Elevator elevator;
+        private final Outtake outtake;
+        // private RobotConfig config;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
-  public RobotContainer() {
-    algaePivot = new AlgaePivot();
-    algaeIntake  = new AlgaeIntake();
-    drivetrain = new Drive();
-    // Configure the trigger bindings
-    configureBindings();
-    // climb = new Climb();
-    elevating = new Elevating();
-    outtake = new Outtake();
-    intake = new Intake();
-    coralTransition = new CoralTransition(intake, outtake);
-    configureDefaultCmds();
+        // private SendableChooser<Command> autonChooser;
 
-  }
+        /**
+         * The container for the robot. Contains subsystems, OI devices, and commands.
+         */
+        public RobotContainer() {
+                // Configure the trigger bindings
+                drivetrain = new Drive();
+                configureDefaultCmds();
+                configureBindings();
+                outtake = new Outtake();
+                elevator = new Elevator();
+        }
 
-  private void configureDefaultCmds(){
-    algaePivot.setProcessorCmd();
+        private void configureDefaultCmds() {
+                
+                // drivetrain.setDefaultCommand(
+                // () -> drivetrain.drive(
+                // () -> MathUtil.applyDeadband(-driveJoy.getLeftY(), 0.1),
+                // () -> MathUtil.applyDeadband(-driveJoy.getLeftX(), 0.1),
+                // () -> MathUtil.applyDeadband(-driveJoy.getRightX(), 0.1)));
 
-    drivetrain.setDefaultCommand(
+                drivetrain.setDefaultCommand(
                         new RunCommand(() -> drivetrain.drive(
                                 () -> MathUtil.applyDeadband(-driveJoy.getLeftY(), 0.1),
                                 () -> MathUtil.applyDeadband(-driveJoy.getLeftX(), 0.1),
                                 () -> MathUtil.applyDeadband(-driveJoy.getRightX(), 0.1)), drivetrain));
-  }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
-  private void configureBindings() {
+        
+                
 
-    //algaeintake
+                // algaePivot.setDefaultCommand(algaePivot.setProcessorCmd());
+                elevator.setDefaultCommand(
+                        new RunCommand(() -> elevator.setLevel(Constants.ElevatorConstants.SetpointConstants.FIRST_LVL), elevator));
+        }
 
-    operJoy.rightBumper()
-      .whileTrue(algaeIntake.reverseRollersCmd().alongWith(algaePivot.setGroundCmd()))//.alongWith(algaePivot.setGroundCmd()))
-      .onFalse(algaeIntake.stopRollersCmd());
+        // public void configureAuton(){
 
-    operJoy.leftBumper()
-      .whileTrue(algaeIntake.runRollersCmd())
-      .onFalse(algaeIntake.stopRollersCmd());
-    
-    operJoy.leftTrigger()
-      .whileTrue(algaePivot.setGroundCmd());
-    
-    operJoy.rightTrigger()
-      .whileTrue(algaePivot.setProcessorCmd());
+        // // // Configure AutoBuilder last
+        // // AutoBuilder.configure(drivetrain.getPose(), // Robot pose supplier
+        // // drivetrain.resetOdometry(drivetrain.getPose()), // Method to reset
+        // odometry (will be called if your auto has a starting pose)
+        // // drivetrain.getChassisSpeeds(), // ChassisSpeeds supplier. MUST BE ROBOT
+        // RELATIVE
+        // // (speeds,feedforwards)->
 
-    
-    algaePivot.setDefaultCommand(algaePivot.setProcessorCmd());
-    
-    //coralouttake
+        // // drivetrain.autoDrive(speeds), // Method that will drive the robot given
+        // ROBOT RELATIVE ChassisSpeeds. Also optionally outputs individual module
+        // feedforwards
+        // // new PPHolonomicDriveController( // PPHolonomicController is the built in
+        // path following controller for holonomic drive trains
+        // // new PIDConstants(5.0, 0.0, 0.0), // Translation PID constants
+        // // new PIDConstants(5.0, 0.0, 0.0) // Rotation PID constants
+        // // ),
+        // // config, // The robot configuration
+        // // () -> {
+        // // // Boolean supplier that controls when the path will be mirrored for the
+        // red alliance
+        // // // This will flip the path being followed to the red side of the field.
+        // // // THE ORIGIN WILL REMAIN ON THE BLUE SIDE
 
-    operJoy.a()
-      .whileTrue(elevating.firstLevelCmd());
-    
-    operJoy.b()
-      .whileTrue(elevating.secondLevelCmd());
+        // // var alliance = DriverStation.getAlliance();
+        // // if (alliance.isPresent()) {
+        // // return alliance.get() == DriverStation.Alliance.Red;
+        // // }
+        // // return false;
+        // // },
+        // // drivetrain // Reference to this subsystem to set requirements
+        // // ),
 
-    operJoy.y()
-      .whileTrue(elevating.thirdLevelCmd());
+        // // AutoBuilder.configure(null, null, null, null, null, config, null, null);
 
-    operJoy.x()
-      .whileTrue(elevating.fourthLevelCmd());
-    
-  //   //algaeremoval
+        // }
 
-    operJoy.back()
-      .whileTrue(elevating.algaeSecondLevelCmd());
-    
-    operJoy.start() 
-      .whileTrue(elevating.algaeThirdLevelCmd());
+        /**
+         * Use this method to define your trigger->command mappings. Triggers can be
+         * created via the
+         * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+         * an arbitrary
+         * predicate, or via the named factories in {@link
+         * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+         * {@link
+         * CommandXboxController
+         * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+         * PS4} controllers or
+         * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+         * joysticks}.
+         */
+        private void configureBindings() {
+                driveJoy.a()
+                                .whileTrue(
+                                                drivetrain.driveQuasistatic(SysIdRoutine.Direction.kForward));
 
-  //   //reset default
-    operJoy.leftTrigger()
-      .whileTrue(elevating.resetDefault());
-    
-  //   //transition intake to outtake
-    operJoy.rightTrigger()
-      .whileTrue(coralTransition.moveCoralToOuttake());
+                driveJoy.b()
+                                .whileTrue(
+                                                drivetrain.driveQuasistatic(SysIdRoutine.Direction.kReverse));
 
-    operJoy.left
+                driveJoy.x()
+                                .whileTrue(
+                                                drivetrain.driveDynamic(SysIdRoutine.Direction.kForward));
 
-  //   // run climb spool
-  //   operJoy.povUp()
-  //     .whileTrue(climb.pulleySystemCmd());
-    // operJoy.rightBumper()
-    //   .whileTrue(algaeIntake.runRollersCmd())
-    //   .onFalse(algaeIntake.stopRollersCmd());
+                driveJoy.y()
+                                .whileTrue(
+                                                drivetrain.driveDynamic(SysIdRoutine.Direction.kReverse));
 
-    // operJoy.leftBumper()
-    //   .whileTrue(algaeIntake.reverseRollersCmd())
-    //   .onFalse(algaeIntake.stopRollersCmd());
+                driveJoy.leftBumper()
+                                .whileTrue(
+                                                drivetrain.setXCmd());
 
-    
-    
-  }
+                driveJoy.rightBumper()
+                                .whileTrue(
+                                                drivetrain.resetGyro());
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return null;
-  }}
+                driveJoy.leftTrigger()
+                                .whileTrue(
+                                                drivetrain.setStraightCmd());
 
+                driveJoy.rightTrigger()
+                                .whileTrue(
+                                                drivetrain.driveStraightCmd());
+
+                operJoy.leftStick()
+                                .whileTrue(
+                                                outtake.reverseOuttakeCmd());
+
+                operJoy.povUp()
+                                .whileTrue(
+                                               elevator.runMotorCmd());
+
+                operJoy.povDown()
+                                .whileTrue(
+                                                elevator.reverseRunMotorCmd());
+                
+        }
+
+        /**
+         * Use this to pass the autonomous command to the main {@link Robot} class.
+         * 
+         * @return the command to run in autonomous
+         */
+
+        // public void configureAuton(){
+        // AutoBuilder.configureHolonomic(
+        // drive::getPose,
+        // drive::resetOdometry,
+        // drive::getRobotRelativeChassisSpeeds, //chassis speed supplier must be robot
+        // relative
+        // drive::setChassisSpeeds, //method that will drive the robot based on robot
+        // relative chassis speed
+        // new HolonomicPathFollowerConfig(
+        // new com.pathplanner.lib.config.PIDConstants(Drive.kP, Drive.kI, Drive.kD), //
+        // Translation PID constants
+        // new com.pathplanner.lib.config.PIDConstants(Turning.kP, Turning.kI,
+        // Turning.kD), // Rotation PID constants
+        // DriveConstants.MAX_SPEED, // Max module speed, in m/s
+        // ModuleConstants.WHEEL_DIAMETER/2,
+        // new ReplanningConfig()),
+        // () -> {
+        // var alliance = DriverStation.getAlliance();
+        // if (alliance.isPresent()) {
+        // return alliance.get() == DriverStation.Alliance.Red;
+        // }
+        // return false;
+        // },
+        // drive);
+
+        // }
+
+        public Command getAutonomousCommand() {
+                // An example command will be run in autonomous
+                return null;
+        }
+
+}
