1de1cf21e8d1e0ba6fed582652a5266eea2b8ae3
Jacqueline Ni <nij@bxscience.edu>
2025-03-11 20:43:31 -0400

    moved urcl.start from constructor to teleopInit

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import org.littletonrobotics.urcl.URCL;
+
 import com.ctre.phoenix6.SignalLogger;
 
 import edu.wpi.first.cameraserver.CameraServer;
@@ -40,7 +42,6 @@ public class Robot extends TimedRobot implements Logged {
     boolean lazyLogging = false;
     Monologue.setupMonologue(this, "Robot", fileOnly, lazyLogging);
     DataLogManager.start();
-    // URCL.start();
     //CameraServer.startAutomaticCapture(); 
     // PortForwarder.add(5800, "10.22.65.11", 5800);
   }
@@ -96,6 +97,7 @@ public class Robot extends TimedRobot implements Logged {
     // teleop starts running. If you want the autonomous to
     // continue until interrupted by another command, remove
     // this line or comment it out.
+    URCL.start();
     SignalLogger.start();
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
